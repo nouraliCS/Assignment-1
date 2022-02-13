67f707232c4ac055465b8010331d63788e912468
nouraliSDSU <nhali@sdsu.edu>
2022-02-13 13:26:15 -0800

    "latest update"

--- public/App.js
+++ public/App.js
@@ -1,4 +1,29 @@
-const element = /*#__PURE__*/React.createElement("div", {
-  title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
-ReactDOM.render(element, document.getElementById('contents'));
+"use strict";
+
+var student_name = /*#__PURE__*/React.createElement("div", {
+  id: "name"
+}, /*#__PURE__*/React.createElement("h2", {
+  id: "name_comp"
+}, "Nour Ali"));
+var pic = /*#__PURE__*/React.createElement("div", {
+  id: "picture"
+}, /*#__PURE__*/React.createElement("img", {
+  id: "picture_comp",
+  src: "./assets/nour.png"
+}));
+var introduction = /*#__PURE__*/React.createElement("div", {
+  id: "intro"
+}, /*#__PURE__*/React.createElement("p", {
+  id: "paragraph"
+}, "My name is Nour Ali. I love watching Anime. Listening to holy music and watching TV in general."));
+var button = /*#__PURE__*/React.createElement("div", {
+  id: "button"
+}, /*#__PURE__*/React.createElement("a", {
+  href: "https://github.com/nouraliCS/Assignment-1"
+}, /*#__PURE__*/React.createElement("button", {
+  id: "button_comp"
+}, "VIEW MY GITHUB REPO")));
+ReactDOM.render(student_name, document.getElementById('name'));
+ReactDOM.render(pic, document.getElementById('picture'));
+ReactDOM.render(introduction, document.getElementById('introduction'));
+ReactDOM.render(button, document.getElementById('button'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-const introduction = (
-    <div title="Outer div">
-      <h1>Hello World!</h1>
-    </div>
+import styles from './public/styles.css'
+const student_name = (
+  <div class ="name">
+          <h2 id = "h2_name">Nour Ali</h2>
+  </div>
   );
   
-  ReactDOM.render(introduction, document.getElementById('contents'));
+  const pic = (
+      <div class = "picture">
+          <img src="https://i.ibb.co/Tkg7Prx/nour.png" alt="nour" border="0">
+      </div>
+  );
+  
+  const introduction = (
+      <div class= "intro">
+          <p id = "paragraph">
+            My name is Nour Ali. I love watching Anime. Listening to holy music and watching TV in general.
+          </p>
+      </div>
+              
+  );
+  
+  const button = (
+      <div class= "button">
+          <a href="https://github.com/nouraliCS/Assignment-1"><button id= "button_comp">VIEW MY GITHUB REPO</button></a>
+      </div>
+  
+  );
+  
+  ReactDOM.render(student_name, document.getElementById('name'));
+  ReactDOM.render(pic, document.getElementById('picture'));
+  ReactDOM.render(introduction, document.getElementById('introduction'));
+  ReactDOM.render(button, document.getElementById('button'));
+  
 
 
   
