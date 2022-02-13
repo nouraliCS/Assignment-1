632fa45f35bf6e08b46cd99e72c04131f3ec27d4
nouraliSDSU <nhali@sdsu.edu>
2022-02-13 13:35:45 -0800

    changes for syles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import styles from './public/styles.css'
+import styles from './public/style.css'
 const student_name = (
   <div class ="name">
-          <h2 id = "h2_name">Nour Ali</h2>
+          <h2>Nour Ali</h2>
   </div>
   );
   
@@ -13,7 +13,7 @@ const student_name = (
   
   const introduction = (
       <div class= "intro">
-          <p id = "paragraph">
+          <p>
             My name is Nour Ali. I love watching Anime. Listening to holy music and watching TV in general.
           </p>
       </div>
@@ -22,7 +22,7 @@ const student_name = (
   
   const button = (
       <div class= "button">
-          <a href="https://github.com/nouraliCS/Assignment-1"><button id= "button_comp">VIEW MY GITHUB REPO</button></a>
+          <a href="https://github.com/nouraliCS/Assignment-1"><button>VIEW MY GITHUB REPO</button></a>
       </div>
   
   );
